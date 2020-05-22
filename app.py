e2bf32de1dad9c40010a8ef1fce332c038a009a6
Adam <adamsmietanka@outlook.com>
2020-05-22 15:58:49 +0200

    Basic backend validation added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from forms import TextForm, RadioForm, TypeDataForm
 from calc import PropellerVariable, PropellerFixed
 from flask_bootstrap import Bootstrap
@@ -21,22 +21,28 @@ def index():
     text_inputs = TextForm(request.form)
     radio_inputs = RadioForm(request.form)
     if request.method == 'POST':
-        if radio_inputs.blade_type.data == 'var':
-            prop = PropellerVariable(text=text_inputs,
-                                     radio=radio_inputs,
-                                     type_data=type_data)
-            figs = [(prop.draw_eff(), 'scatter1'), (prop.draw_angle(), 'scatter2')]
+        if text_inputs.validate() and type_data.validate() and radio_inputs.blades.data in [2, 3, 4]:
+            if radio_inputs.blade_type.data == 'var':
+                prop = PropellerVariable(text=text_inputs,
+                                         radio=radio_inputs,
+                                         type_data=type_data)
+                figs = [(prop.draw_eff(), 'scatter1'), (prop.draw_angle(), 'scatter2')]
+            else:
+                prop = PropellerFixed(text=text_inputs,
+                                      radio=radio_inputs,
+                                      type_data=type_data)
+                figs = [(prop.draw_eff(), 'scatter1')]
+            return render_template('calc.html',
+                                   table=prop.data.to_html(classes=['table', 'table-striped'], header="true"),
+                                   type_data=type_data,
+                                   text=text_inputs,
+                                   radio=radio_inputs,
+                                   figs=figs)
         else:
-            prop = PropellerFixed(text=text_inputs,
-                                  radio=radio_inputs,
-                                  type_data=type_data)
-            figs = [(prop.draw_eff(), 'scatter1')]
-        return render_template('calc.html',
-                               table=prop.data.to_html(classes=['table', 'table-striped'], header="true"),
-                               type_data=type_data,
-                               text=text_inputs,
-                               radio=radio_inputs,
-                               figs=figs)
+            return render_template('error.html',
+                                   type_data=type_data,
+                                   text=text_inputs,
+                                   radio=radio_inputs)
     else:
         return render_template('index.html',
                                type_data=type_data,
