539fede376d47fbc895460155b7e6931823ae1a2
Adam <adamsmietanka@outlook.com>
2021-03-17 15:43:04 +0100

    feat: switch results calculation to Lambda

--- src/store/modules/results.js
+++ src/store/modules/results.js
@@ -8,9 +8,6 @@ export default {
       altitude: 0,
       power: 800,
     },
-    hovered: {
-      color: [],
-    },
     table: [],
     charts: {},
   },
@@ -43,16 +40,18 @@ export default {
       commit('SET_POWER', roundPower(rootState.engine, parseFloat(altitude)));
     },
     async postData({ dispatch }, params) {
-      axios.get('https://k2yzx7le7i.execute-api.eu-central-1.amazonaws.com/dev/prop_results_variable', { params })
-      .then((res) => {
+      let request;
+      if (params.pitch === 'Variable') {
+        request = axios.get('https://k2yzx7le7i.execute-api.eu-central-1.amazonaws.com/dev/prop_results_variable', { params })
+      } else {
+        request = axios.get('https://0dog08xcmk.execute-api.eu-central-1.amazonaws.com/default/results_fixed', { params })
+      }
+      request.then((res) => {
         let data = JSON.stringify(res.data, (_key, val) => val.toFixed ? Number(val.toPrecision(4)) : val);
         data = JSON.parse(data)
         dispatch('setTable', data.table);
+        dispatch('setCharts', data.charts);
       });
-      axios.post('/api/data', params)
-        .then((response) => {
-          dispatch('setCharts', response.data.charts);
-        });
     },
     setTable({ commit }, table) {
       commit('SET_TABLE', table);
